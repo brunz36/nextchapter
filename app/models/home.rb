31f733d76deebea70a5809a0dcf6d402fe80aed3
Bruno Ribeiro <brunz36@gmail.com>
2017-04-16 19:05:26 -0400

    Added validations

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,16 @@
 class Home < ApplicationRecord
+  validates :address, :city, length: { in: 5..60 }
+  validates :state, length: { is: 2 }
+  validates :zip, length: { is: 5 }, numericality: true
+  validates :number_bedrooms, :number_bathrooms, length: { in: 1..2 }, numericality: true
+  validates :price, length: { in: 4..10 }, numericality: true
+  validates :description, length: { in: 0..5000 }
+  validates :roof, :city, length: { in: 2..30 }
+  validates :lot_size, length: { in: 2..10 }, numericality: true
+  validates :hoa, inclusion: { in: [true, false] }
+  validates :number_of_floors, length: { is: 1 }, numericality: true
+  validates :year_built, length: { is: 4 }, numericality: true
+
   include ImageUploader[:image]
 
   belongs_to :created_by, class_name: "User"
